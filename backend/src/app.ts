1df34b5914b0c2aa7c7d3efb0d12a5968bb0849c
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2024-02-02 21:07:01 -0500

    added readmes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import example from "./routes/example";
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 
-app.use("/api/example", example)
+app.use("/api/example", example);
 
 app.listen(3001, () => {
     console.log("started");
